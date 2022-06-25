9bc5c3d0598e22a1d9f023add3c89eb71b71a7b3
kikuchi takuya <kiiro.yellow.amarero@gmail.com>
2022-06-25 22:11:55 +0900

    GUI化

---
+++ original_module/md_maker.py
@@ -0,0 +1,27 @@
+import markdown
+
+def md_making(md_url, css_url):
+
+    # 必要ファイルの読み込み
+    with open('./template/doctype.html', 'r', encoding='utf-8') as doc:
+        doc_file = doc.read()
+    html = doc_file +'\n'
+
+    with open('./template/head.html', 'r', encoding='utf-8') as head:
+        head_file = head.read()
+    html += '<head>' + head_file + '\n' 
+    html += '<link rel="stylesheet" href='+ css_url + '>'
+    html += '</head>' + '\n'
+
+    # マークダウン読み込み
+    with open(md_url, 'r', encoding='utf-8') as input_file:
+        read_file = input_file.read()
+    # マークダウンをhtmlに変換
+    html += '<body>\n' + markdown.markdown(read_file) + '\n</body>\n'
+
+    # </html>挿入
+    html += '</html>'
+
+    # htmlをhtmlファイルに書き込み
+    with open('test.html', 'w', encoding='utf-8', errors='xmlcharrefreplace') as output_file:
+        output_file.write(html)
--- py_markdown.py
+++ py_markdown.py
@@ -1,25 +1,63 @@
-import markdown
+# 標準ライブラリ
+import tkinter as tk
+import tkinter.ttk as ttk
+import tkinter.filedialog as tf
 
-# 必要ファイルの読み込み
-with open('./template/doctype.html', 'r', encoding='utf-8') as doc:
-    doc_file = doc.read()
-html = doc_file +'\n'
+# 自作ライブラリ
+from original_module import md_maker
 
-with open('./template/head.html', 'r', encoding='utf-8') as head:
-    head_file = head.read()
-html += '<head>' + head_file + '\n' 
-html += '<link rel="stylesheet" href='+ './template/py_markdown.css' + '>'
-html += '</head>' + '\n'
+# 各種ウィジットの関数
+def find_file(tp):
+    type = [('', tp)]
+    dir = 'C:'
+    path = tf.askopenfilenames(filetypes=type, initialdir=dir)
+    if not path:
+        return
+    print(path)
+    return path
 
-# マークダウン読み込み
-with open('test.md', 'r', encoding='utf-8') as input_file:
-    read_file = input_file.read()
-# マークダウンをhtmlに変換
-html += '<body>\n' + markdown.markdown(read_file) + '\n</body>\n'
+def find_md(event):
+    path_txt = find_file('*.md')
+    md_entry.delete(0, tk.END)
+    md_entry.insert(tk.END, str(path_txt[0]))
 
-# </html>挿入
-html += '</html>'
+def find_css(event):
+    path_txt = find_file('*.css')
+    css_entry.delete(0, tk.END)
+    css_entry.insert(tk.END, str(path_txt[0]))
 
-# htmlをhtmlファイルに書き込み
-with open('test.html', 'w', encoding='utf-8', errors='xmlcharrefreplace') as output_file:
-    output_file.write(html)
+def exe(event):
+    md_maker.md_making(md_entry.get(), css_entry.get())
+
+# rootメインウィンドウの設定
+root = tk.Tk()
+root.title("Markdown Maker")
+root.geometry("600x400")
+
+# メインフレームの作成と設置
+frame = ttk.Frame(root)
+frame.grid(column=0, row=0, sticky=tk.NSEW, padx=5, pady=10)
+
+# 各種ウィジットの作成
+md_entry = ttk.Entry(frame)
+md_read_btn = ttk.Button(frame, text="Markdown読込")
+md_read_btn.bind("<Button-1>", find_md)
+
+css_entry = ttk.Entry(frame)
+css_read_btn = ttk.Button(frame, text="css読込")
+css_read_btn.bind("<Button-1>", find_css)
+
+exe_btn = ttk.Button(frame, text="実行")
+exe_btn.bind("<Button-1>", exe)
+
+# 各種ウィジットの設置
+md_entry.grid(row=0, column=0)
+md_read_btn.grid(row=0, column=1)
+css_entry.grid(row=1, column=0)
+css_read_btn.grid(row=1, column=1)
+exe_btn.grid(row=2, column=1)
+
+
+
+
+root.mainloop()
